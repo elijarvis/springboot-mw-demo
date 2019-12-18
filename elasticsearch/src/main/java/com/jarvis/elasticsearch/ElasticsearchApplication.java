b49933d44b24cb5f4731be3d8a7718e8261e9f4e
elijarvis <elijarvis@163.com>
2019-12-18 11:40:01 +0800

    first commit

---
+++ elasticsearch/src/main/java/com/jarvis/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,18 @@
+package com.jarvis.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jarvis
+ * @param
+ * @return
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
